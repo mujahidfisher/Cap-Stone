50481202740ca871656ce0d871b0c259e7350b0f
mujahidfisher <fishermujahid@gmail.com>
2023-08-30 15:45:36 +0200

    checkout html done

---
+++ frontend/src/components/checkoutTableComp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <button>Clear</button>
+      <table class="table table-striped">
+        <h4>Your Listing:</h4>
+        <thead>
+          <tr>
+            <th scope="col">Item Name</th>
+            <th scope="col">Tag</th>
+            <th scope="col">Image</th>
+            <th scope="col">Price</th>
+            <th scope="col">Remove or Purchase</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td><button>Remove</button> <button>Purchase</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
