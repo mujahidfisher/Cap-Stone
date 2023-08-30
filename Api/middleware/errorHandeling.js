299cbc713d2682afe4b930a600700f031e8b29df
mujahidfisher <fishermujahid@gmail.com>
2023-08-30 11:40:18 +0200

    config, controller, middleware

---
+++ Api/middleware/errorHandeling.js
@@ -0,0 +1,12 @@
+function errorHandeling(err, req, res, next) {
+    if(err) {
+        let status = err.statusm || 500
+        res.json({
+            status,
+            msg: "An error occurred; please try again later."
+        })
+    }
+next()
+}
+
+module.exports = errorHandeling
