dfc870f5a34d529c45f3c3604aa4a56f1eca8d1d
mujahidfisher <fishermujahid@gmail.com>
2023-08-30 12:32:47 +0200

    routers to all pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/merch">Merchandise</router-link> |
+    <router-link to="/checkout">Checkout</router-link> |
+    <router-link to="/admin">Admin</router-link> |
+    <router-link to="/contact">Contact</router-link>
   </nav>
-  <!-- <router-view/> -->
+  <!-- <router-view /> -->
 </template>
 
 <style>
