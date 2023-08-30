a908551d79b4462ce43368734d91403032fe3300
mujahidfisher <fishermujahid@gmail.com>
2023-08-30 14:07:34 +0200

    footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,17 @@
   <div>
     <navBarComp />
     <router-view />
+    <footerCompVue />
   </div>
 </template>
 
 <script>
 import navBarComp from './components/navBarComp.vue';
+import footerCompVue from './components/footerComp.vue';
   export default {
     components: {
       navBarComp,
+      footerCompVue
     }
   }
 </script>
--- frontend/src/components/footerComp.vue
+++ frontend/src/components/footerComp.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-
+  <div>
+    <div class="footer">
+      <div class="top-container-fluid">
+        <div class="row">
+            <div class="col">
+                <p>Community</p>
+                <p><a href=""></a></p>
+                <p><a href=""></a></p>
+            </div>
+            <div class="col">
+                <img src="" alt="logoFooter">
+            </div>
+            <div class="col">
+                <p>Cape Town</p>
+                <p>South Africa</p>
+            </div>
+        </div>
+      </div>
+      <div class="bottom-container-fluid">
+        <div class="row">
+            <div class="col">
+                <p>CopyRight &copy; MujahidFisher</p>
+            </div>
+            <div class="col">
+                <p>Rights Reserved &reg; 2023</p>
+            </div>
+            <div class="col">
+                <a href=""><img src="" alt="icon"></a>
+                <a href=""><img src="" alt="icon"></a>
+                <a href=""><img src="" alt="icon"></a>
+                <a href=""><img src="" alt="icon"></a>
+            </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
