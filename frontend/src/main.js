2e76d6b50849c0ca23f511616cd00d3ba9b02576
mujahidfisher <fishermujahid@gmail.com>
2023-08-30 12:16:45 +0200

    css, deleted unused files

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import css from "./assets/CSS/style.css"
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(css).use(router).mount('#app')
