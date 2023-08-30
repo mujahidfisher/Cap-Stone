7dd2fa8a6875223e534a8be1c2b17cb5433974db
mujahidfisher <fishermujahid@gmail.com>
2023-08-30 14:53:14 +0200

    about html done

---
+++ frontend/src/components/section2AboutComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="container-fluid">
+            <h2>So who's the Founder:</h2>
+            <img src="" alt="profile">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fuga consequuntur aliquid omnis dolores consequatur ea. Similique omnis nam fuga ducimus nisi libero sequi error officia itaque dolores. Magnam, a.</p>
+            <button>Contact Me</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <h1>About Page</h1>
-
+    <section1AboutComp />
+    <section2AboutComp />
   </div>
 </template>
 
 <script>
+import section1AboutComp from '@/components/section1AboutComp.vue';
+import section2AboutComp from '@/components/section2AboutComp.vue'
   export default {
-    
+    components: {
+      section1AboutComp,
+      section2AboutComp
+    }
   }
 </script>
 
