2081f20ce2298b004b7c4caee9193acc60f05ccf
mujahidfisher <fishermujahid@gmail.com>
2023-08-31 14:03:54 +0200

    navbar css done

--- frontend/src/components/navBarComp.vue
+++ frontend/src/components/navBarComp.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
+      <div class="container-fluid nav">
         <a class="navbar-brand" href="#">
-          <img class="logo" src="" alt="Logo" loading="lazy"
+          <img class="logo" src="https://i.postimg.cc/c4y2MkJw/d9260ceb4bccd08286f7ee416f4075a3-removebg-preview.png" alt="Logo" loading="lazy"
         /></a>
         <button
           class="navbar-toggler"
@@ -18,8 +18,8 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link">Home</router-link>
+            <li class="nav-item active">
+              <router-link to="/" class="nav-link ">Home</router-link>
             </li>
             <li class="nav-item">
               <router-link to="/about" class="nav-link">About</router-link>
@@ -37,7 +37,7 @@
               <router-link to="/contact" class="nav-link">Contact Me</router-link>
             </li>
             <li>
-                <router-link to="/logIn"><button><img src="" alt="login">LogIn</button></router-link>
+                <router-link to="/logIn"><button class="log-btn"><img src="" alt="login">LogIn</button></router-link>
             </li>
           </ul>
         </div>
