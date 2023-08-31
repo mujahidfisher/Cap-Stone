b80c5c229a61fd3e2a604c4661a8d29b0e867c68
mujahidfisher <fishermujahid@gmail.com>
2023-08-31 16:25:40 +0200

    section 1 home page

--- frontend/src/components/section1HomeComp.vue
+++ frontend/src/components/section1HomeComp.vue
@@ -1,14 +1,16 @@
 <template>
-    <div>
+    <div class="background-body">
         <div class="container-fluid">
             <div class="row">
                 <div class="col">
-                    <h3>Welcome to <br> a whole new world</h3>
-                    <h2>Adventurer</h2>
-                    <button>Explore</button>
+                    <div class="textHome glitch hero layers" data-text="呂ヨ卞　尺ヨ丹句と　乍回尺　卞廾ヨ　几ヨメ卞　呂ヨ几　丹几工冊ヨ　山ヨ日己工卞ヨ">
+                        <h1>Welcome to <br> a whole new world <br>
+                        Adventurer</h1>
+                        <button class="explore-btn">Explore</button>
+                    </div>
                 </div>
                 <div class="col">
-                    <img src="" alt="homeImg">
+                    <img class="itachi" src="https://i.postimg.cc/PrxGvPf9/14018.gif" alt="homeImg">
                 </div>
             </div>
         </div>
