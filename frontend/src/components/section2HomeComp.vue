40ad3df727d7559c1564842414599255c171e323
mujahidfisher <fishermujahid@gmail.com>
2023-09-01 16:18:23 +0200

    images add

--- frontend/src/components/section2HomeComp.vue
+++ frontend/src/components/section2HomeComp.vue
@@ -1,32 +1,11 @@
 <template>
-    <div>
-        <div class="container-fluid">
-            <div class="card-1">
-                pic1
-
-            </div>
-            <div class="card-2">
-                pic2
-
-            </div>
-            <div class="card-3">
-                pic3
-
-            </div>
-            <div class="card-4">
-                pic4
-
-            </div>
-        </div>
-    </div>
+  <div class="box-gallery">
+    <img src="" alt="">
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
