b9057f33df3b7ef082586714b8a2918ffdef2e19
mujahidfisher <fishermujahid@gmail.com>
2023-09-04 10:25:04 +0200

    edit

--- frontend/src/components/section2HomeComp.vue
+++ frontend/src/components/section2HomeComp.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="box-gallery">
-    <img src="" alt="">
+  <div class="section2">
+
+    <div class="box-gallery">
+      <div class="box">
+        <img class="merchI-1" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwbd2a8b94/Apparel/TSM587ZCRURE%20-%20Chainsaw%20Man%20-%20Power%20Katakana%20T-Shirt/TSM587ZCRU_1.jpg?sw=300&sh=300&sm=fit" alt="pic2">
+      </div>
+      <div class="box">
+        <img class="merchI-2" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dw5bf773bb/images/4580779538288_jujutsu-kaisen-itadori-choso-dararins-medium-blind-plush-4_1.jpg?sw=300&sh=300&sm=fit" alt="pic3">
+      </div>
+      <div class="box">
+        <img class="merchI-3" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwe0b0e13a/Home%20Video/704400109164/MarriedCouplebutNotLovers_US-ANZ_Spread.jpg?sw=300&sh=300&sm=fit" alt="pic4">
+      </div>
+      <div class="box">
+        <img class="merchI-4" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwfb7debab/Figures/4595643112051/4595643112051_figure-shizuka-mikazuki-zom-100-bucket-list-of-the-dead-alta.jpg?sw=300&sh=300&sm=fit" alt="pic1">
+      </div>
+      <div class="box">
+        <img class="merchI-5" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dw6a993467/Figures/4580416948425/4580416948425%20(1).jpg?sw=300&sh=300&sm=fit" alt="pic1">
+      </div>
+      <div class="box">
+        <img class="merchI-6" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dw20749520/images/4580590181618_konosuba-aqua-kuripan-plush_1.jpg?sw=300&sh=300&sm=fit" alt="pic1">
+      </div>
+      <div class="box">
+        <img class="merchI-7" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dw5b1e4ae8/Figures/ImagesFuni/unassigned/fun-00145-1.jpg?sw=300&sh=300&sm=fit" alt="pic1">
+      </div>
+      <div class="box">
+        <img class="merchI-8" src="https://store.crunchyroll.com/dw/image/v2/BDGC_PRD/on/demandware.static/-/Sites-crunchyroll-master-catalog/default/dwc030538f/Figures/4580416948364/4580416948364%20(1).jpg?sw=300&sh=300&sm=fit" alt="pic1">
+      </div>
+    </div>
+    <div class="disco-btn">
+      <button class="disco">Discover</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
 <style scoped></style>
