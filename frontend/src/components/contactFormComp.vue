0c3efb4ec9107bba624c5d6d2d1b1ce929495876
mujahidfisher <fishermujahid@gmail.com>
2023-09-05 16:07:47 +0200

    contact page

--- frontend/src/components/contactFormComp.vue
+++ frontend/src/components/contactFormComp.vue
@@ -1,14 +1,54 @@
 <template>
   <div>
-    <div class="container-fluid">
-      <form action="https://formspree.io/f/mrgwkzjn" method="POST">
-        <input type="text" name="Name" placeholder="Name">
-        <input type="text" name="Name" placeholder="Surname">
-        <input type="email" name="email" placeholder="Email Address" />
-        <textarea name="message" placeholder="Message"></textarea>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form action="https://formspree.io/f/mrgwkzjn" method="POST">
+      <div class="form" name="form">
+        <input
+          type="text"
+          class="form-control"
+          autocomplete="off"
+          placeholder="First name"
+          name="First name"
+          required
+          oninvalid="this.setCustomValidity('Whats your Name!')"
+          oninput="setCustomValidity('')"
+        />
+  
+        <input
+          type="text"
+          class="form-control"
+          autocomplete="off"
+          placeholder="Last name"
+          name="Last name"
+          required
+          oninvalid="this.setCustomValidity('I need a Surname!')"
+          oninput="setCustomValidity('')"
+        />
+  
+        <input
+          type="email"
+          class="form-control"
+          placeholder="Email:"
+          autocomplete="off"
+          name="email"
+          required
+          oninvalid="this.setCustomValidity('Email is required as Usual!')"
+          oninput="setCustomValidity('')"
+        />
+  
+        <textarea
+          type="text"
+          class="form-control"
+          placeholder="Message:"
+          name="message"
+          autocomplete="off"
+          required
+          oninvalid="this.setCustomValidity('Dont you want to say anything!')"
+          oninput="setCustomValidity('')"
+        ></textarea>
+  
+        <button class="btn btn-outline-warning" type="submit">SUBMIT</button>
+      </div>
+    </form>
   </div>
 </template>
 
