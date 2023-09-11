b39ea65a23bea13f9ff914b01c6f9f5f2df8cb05
mujahidfisher <fishermujahid@gmail.com>
2023-09-11 16:35:53 +0200

    filters

--- frontend/src/components/singleItem.vue
+++ frontend/src/components/singleItem.vue
@@ -1,49 +1,16 @@
 <template>
-  <div>
+  <div class="single-view">
     <div class="ImgSingle">
-      <img :src="$route.query.img" :alt="$route.query.img" loading="lazy" />
+      <img class="singleImg img-fluid" :src="$route.query.img" :alt="$route.query.img" loading="lazy" />
     </div>
-
-    <div class="col">
-      <div class="col">
-        <div class="thumbnail-container">
-          <img
-            class="drift-demo-trigger"
-            :data-zoom="$route.query.img"
-            :src="$route.query.img"
-          />
-        </div>
-      </div>
-
-      <div class="col">
         <div class="details">
           <h1 class="singleTitle">{{ $route.query.name }}</h1>
-          <p class="price">R{{ $route.query.price }}</p>
-          <p class="tag">{{ $route.query.tag }}</p>
+          <h3 class="singleTitle">R{{ $route.query.price }}</h3>
+          <h5 class="singleTitle">{{ $route.query.tag }}</h5>
+          <button class="contact-btn">Add to cart</button>
+          <router-link class="deleter" to="/merch">Back</router-link>
         </div>
-      </div>
     </div>
-  </div>
-  <!-- <div>
-    <div class="singImg">
-      <div>
-        <img
-          :src="$route.query.img"
-          class="singleImg img-fluid"
-          :alt="$route.query.img"
-          loading="lazy"
-        />
-      </div>
-      <div>
-        <router-link to="/merch" class="mainBack">Back</router-link>
-      </div>
-      <div class="info">
-        <h2>{{ $route.query.name }}</h2>
-        <h4>{{ $route.query.tag }}</h4>
-        <h4>R{{ $route.query.price }}</h4>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script>
